7756b72b82c5399fce0e9b0f883eb1d4a7a00fe2
yoshikif25 <ilk0724yf@yahoo.co.jp>
2020-06-17 19:10:30 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module ChatSpace
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
-
+    config.i18n.default_locale = :ja
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
